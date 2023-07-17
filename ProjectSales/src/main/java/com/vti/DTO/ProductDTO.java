8a38b90a792a0ae470ddea468b25cd08a718cb73
tranquangtu769802 <tranquangtu0498@gmail.com>
2023-07-17 14:15:27 +0700

    booksale

---
+++ ProjectSales/src/main/java/com/vti/DTO/ProductDTO.java
@@ -0,0 +1,29 @@
+package com.vti.DTO;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+public class ProductDTO {
+    private int id;
+
+    private String name;
+
+    private int price;
+
+    private String image;
+
+    private int Star;
+
+    private String typeName;
+
+    private String brandName;
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private Date createDate;
+
+}
